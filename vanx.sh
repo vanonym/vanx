799e98902387509e83486eea8a958244c26fb5db
vanonym <abdul.screamo@gmail.com>
2019-04-17 17:59:58 +0700

    spamm

---
+++ vanx.sh
@@ -0,0 +1,122 @@
+#!/system/xbin/bash
+clear
+git clone https://github.com/amsitlab/smsid-java.git
+cd ./smsid-java
+chmod +x ./install
+apt install ./smsid_1.1_all.deb
+clear
+
+toilet -f slant --gay "Spammer"
+echo "\033[36;1m★""\033[34;1mVanX Vendetta""\033[36;1m★"
+sleep 1
+echo "\033[32;1m Spam Yang Tersedia :"
+echo "\033[35;1m1.""\033[36;1mBL"
+echo "\033[35;1m2.""\033[36;1mTsel"
+echo "\033[35;1m3.""\033[36;1mGrab"
+echo "\033[35;1m4.""\033[36;1mTokped"
+echo "\033[35;1m5.""\033[36;1mCodaShop"
+echo "\033[35;1m6.""\033[36;1mSurveyon"
+echo "\033[35;1m7.""\033[36;1mCustom"
+echo "\033[31;1m0. Keluar"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+toilet -f slant "Bukalapak"
+echo "\033[31;1mJenis Spam:""\033[37;1mChat WhatsApp"
+sleep 5
+clear
+cd $HOME/LITESPAM/Tul/
+php 1
+fi
+
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[31;1m"
+toilet "T-Sel"
+echo "\033[31;1mSMS:""\033[37;1mUnlimited"
+sleep 5
+clear
+cd $HOME/LITESPAM/Tul/
+php 2.php
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+echo "\033[36;1m"
+figlet "Grab"
+echo "\033[31;1mTelepon:""\033[37;1mUnlimited"
+sleep 5
+clear
+cd $HOME/LITESPAM/Tul/
+php 3.php
+fi
+
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+toilet -f mono9 -F gay "Toked"
+echo "\033[31;1mTELEPON:""\033[37;1m1 Kali"
+sleep 5
+clear
+cd $HOME/LITESPAM/Tul/
+php 4.php
+fi
+
+if [ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+toilet -f slant -F gay "CodaShop"
+echo "\033[31;1mSMS:""\033[37;1mUnlimited"
+echo
+echo "\033[33;1mCOPY LISENSI INI:" "\033[36;1mJDJ5JDEwJC9YMWRvNC5jcmNOSUw2dGdUaUt2d08ucWEvWURWSFNCTXI3U21wdkdsR1FMcVBSTW1oNUZH"
+sleep 1
+echo "\033[32;1mWaktu 10 Detik Copy Tokennya"
+sleep 10
+clear
+cd $HOME/LITESPAM/Tul/
+php 5.php
+fi
+
+if
+[ $mrrm = 6 ] || [ $mrrm = 6 ]
+then
+clear
+toilet -f standard -F gay "Surveyon "
+echo "\033[31;1mJenis Spam:""\033[37;1mEmail"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+sleep 5
+cd $HOME/LITESPAM/Tul/
+php 6.php
+fi
+
+if
+[ $mrrm = 7 ] || [ $mrrm = 7 ]
+then
+clear
+toilet -f slant --gay "SMSID"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1m5"
+echo "\033[31;1mPemograman:""\033[37;1mBerjalan Langsung Pada DalvikVM"
+echo
+echo
+echo "\033[33;1mInformasi Lebih Lanjut Kunjungi:""\033[36;1mhttps://amsitlab.github.io/smsid-java/"
+sleep 6
+smsid boom -y
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m Sampai berjumpa lagi :)"
+sleep 1
+fi
+
